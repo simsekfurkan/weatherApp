9f5e9eee6a9262fe4e294eda9e65f73aa310c576
simsekfurkan <furkansimek@gmail.com>
2022-08-05 14:13:22 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Onur Şimşek on 5.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
